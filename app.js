749c27bf6b46bd48516bd207fde1ff6ee52d7131
Yugansh <yuganshdhir608@gmail.com>
2021-06-19 00:43:32 +0530

    second commit

---
+++ app.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const path = require("path");
+const ejsMate = require("ejs-mate");
+
+const app = express();
+
+app.engine("ejs", ejsMate);
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", function (req, res) {
+  res.render("home");
+});
+
+app.get("/videoEditing", function (req, res) {
+  res.render("videoEditing");
+});
+
+app.get("/web", function (req, res) {
+  res.render("web");
+});
+
+app.get("/growIndia", function (req, res) {
+  res.render("growIndia");
+});
+
+app.get("/royaltyish", function (req, res) {
+  res.render("royaltyish");
+});
+
+app.get("/freelancing", function (req, res) {
+  res.render("freelancing");
+});
+
+app.get("*", function (req, res) {
+  res.redirect("/");
+});
+
+app.listen(process.env.PORT || 3000, process.env.IP, function () {
+  console.log("Yugansh Dhir portfolio website is live on port 3000");
+});
